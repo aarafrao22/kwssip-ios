6bd25605170c341bf9cce6a681cad26a199cc4c0
aarafrao22 <aarafrao22@gmail.com>
2024-07-02 12:16:08 +0500

    Splash Added

---
+++ kwssip/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  kwssip
+//
+//  Created by Rao Aaraf Younus on 2024-07-02.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
